c31fd9e60df4af2ba0fc1e5b852dae5e6c56a8b4
dio832 <dio7@txstate.edu>
2026-09-17 19:40:01 -0500

    Create bitshift_tb.v

---
+++ Verilog/bitshift_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ns
+`include "bitshift.v"
+
+module bitshift_tb;
+
+reg signed [3:0] in;
+reg dir;
+wire signed [3:0] out;
+
+bitshift uut(in, dir, out);
+
+initial begin
+
+    $dumpfile("bitshift_tb.vcd");//holds output waveform
+    $dumpvars(0, bitshift_tb);
+
+    in = 4'b0110;
+    dir = 0;
+    #10;
+
+    in = 4'b0110;
+    dir = 1;
+    #10;
+
+    in = 4'b1000;
+    dir = 0;
+    #10;
+
+    in = 4'b1000;
+    dir = 1;
+    #10;
+
+    $display("Testing bitshift");
+
+end
+
+endmodule
